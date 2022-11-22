23ca5a0d6df39d1a154adf2f54a00f257e432a6c
Maria <nathyellemonteiro19@gmail.com>
2022-11-22 15:01:12 -0600

    add sass and google font

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import './App.css'
+import './App.scss'
+import Header from './components/Header/Header'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <div>
+        <Header />
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import styles from './Header.module.scss'
+
+
+const Header = () => <div className={styles.header}><h2>hello world</h2></div>
+
+export default Header
